454e1982153704c0764e31ba24562edb5d08a4db
Maxim Vavilov <max.vavilov99@gmail.com>
2022-02-10 00:53:57 +0300

    Use Redux, create redux-store.js

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -1,7 +1,27 @@
 const SEND_MESSAGE = 'SEND-MESSAGE'
 const UPDATE_NEW_MESSAGE_TEXT = 'UPDATE-NEW-MESSAGE-TEXT'
 
-const dialogsReducer = (state, action) => {
+let initialState = {
+    dialogs : [
+    {id : 1, name:'Maxim'},
+    {id : 2, name:'Alexey'},
+    {id : 3, name:'Alexandr'},
+    {id : 4, name:'Pavel'},
+    {id : 5, name:'Milana'},
+    {id : 6, name:'Olga'},
+],
+    messages : [
+    {id : 1, message:'Hi!'},
+    {id : 2, message:'Sup'},
+    {id : 3, message:'Text me ASAP'},
+    {id : 4, message:'What r u doing?'},
+    {id : 5, message:'See u'},
+    {id : 6, message:'On my way'},
+],
+    newMessageText: '',
+};
+
+const dialogsReducer = (state = initialState, action) => {
     switch (action.type) {
         case SEND_MESSAGE:
             let body = state.newMessageText;

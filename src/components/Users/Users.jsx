bb95a321a043ce38b9c1cb82e33e20fc43de52c6
Maxim Vavilov <max.vavilov99@gmail.com>
2022-02-23 21:26:03 +0300

    Add Users API

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,43 +1,40 @@
+import React from 'react';
 import s from './Users.module.css'
+import * as axios from 'axios';
+import usersPhoto from '../../assets/images/users.png';
 
-let Users = (props) => {    
-    if (props.users.length === 0) {
-        props.setUsers([
-            {id: 1, photoUrl: 'https://cdn-icons-png.flaticon.com/512/147/147140.png', 
-            followed: true, fullName:'Max V', status:'First!', location: {city: 'Grodno', country: 'Belarus',} },
-            {id: 2, photoUrl: 'https://cdn-icons-png.flaticon.com/512/147/147140.png', 
-            followed: false, fullName:'Alex L', status:'Second!', location: {city: 'Grodno', country: 'Belarus',} },
-            {id: 3, photoUrl: 'https://cdn-icons-png.flaticon.com/512/147/147140.png', 
-            followed: false, fullName:'Milana S', status:'Third!', location: {city: 'Grodno', country: 'Belarus',} },
-            {id: 4, photoUrl: 'https://cdn-icons-png.flaticon.com/512/147/147140.png', 
-            followed: false, fullName:'Xander L', status:'I am here too...', location: {city: 'Grodno', country: 'Belarus',} },
-        ])
+class Users extends React.Component {  
+    componentDidMount() {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response =>{   
+            this.props.setUsers(response.data.items)
+        });
     }
-    return <div>
+    render() {
+        return <div>
         {
-            props.users.map(u => <div key={u.id}> 
-                <span>
-                    <div>
-                        <img src={u.photoUrl} className={s.userPhoto}/>
-                    </div>
-                    <div>
-                        <button onClick={() => props.toggleFollow(u.id)}> 
-                            {u.followed ? 'Unfollow' : 'Follow'}
-                        </button>
-                        
-                    </div>
-                </span>
-                <span>
-                    <div>{u.fullName}</div>
-                    <div>{u.status}</div>
-                </span>
-                <span>
-                    <div>{u.location.country}</div>
-                    <div>{u.location.city}</div>
-                </span>
+        this.props.users.map(u => <div key={u.id}> 
+            <span>
+                <div>
+                    <img src={u.photos.small !=null ? u.photos.small : usersPhoto} className={s.userPhoto}/>
+                </div>
+                <div>
+                    <button onClick={() => this.props.toggleFollow(u.id)}> 
+                        {u.followed ? 'Unfollow' : 'Follow'}
+                    </button>
+                </div>
+            </span>
+            <span>
+                <div>{u.name}</div>
+                <div>{u.status}</div>
+            </span>
+            <span>
+                <div>{'u.location.country'}</div>
+                <div>{'u.location.city'}</div>
+            </span>
             </div>)
         }
     </div>
+    }
 }
 
 export default Users;

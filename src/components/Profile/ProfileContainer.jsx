a315e82f3d71951384b167334b7a4929c21caaa0
Maxim Vavilov <max.vavilov99@gmail.com>
2022-05-17 00:54:33 +0300

    Create redirect HOC, add compose

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -4,6 +4,8 @@ import { useMatch } from 'react-router-dom';
 import { Navigate } from 'react-router-dom'
 import Profile from './Profile';
 import { getUserProfile } from '../../redux/profile-reducer';
+import { withAuthRedirect } from '../../hoc/withAuthRedirect';
+import { compose } from 'redux';
 
 
 
@@ -14,19 +16,23 @@ class ProfileContainer extends React.Component {
         this.props.getUserProfile(userId)
     }
     render(){
-        if (!this.props.isAuth) return <Navigate replace to={'/login'} />;
         return <Profile {...this.props} profile={this.props.profile}/>
     }
 }
 
+
+
 let mapStateToProps = (state) => ({
     profile: state.profilePage.profile,
-    isAuth: state.auth.isAuth,
+    //isAuth: state.auth.isAuth,
 })
 
-const ProfileURLMatch = (props) => {
-    const match = useMatch('/profile/:userId/');
-    return <ProfileContainer {...props} match={match} />;
-}
+// const ProfileURLMatch = (props) => {
+//     const match = useMatch('/profile/:userId/');
+//     return <ProfileContainer {...props} match={match} />;
+// }
 
-export default connect(mapStateToProps, {getUserProfile})(ProfileURLMatch);
+export default compose (
+    connect(mapStateToProps, {getUserProfile}),
+    withAuthRedirect) (ProfileContainer)
+
